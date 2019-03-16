35791fdbe68d36a3c950a7e57a0627283470a6c8
TheAllenAnker <theallenanker@gmail.com>
2019-03-16 15:25:02 +0800

    Solve problem: 1001 A+B Format.

--- src/com/allen_anker/Main.java
+++
@@ -1,8 +0,0 @@
-package com.allen_anker;
-
-public class Main {
-
-    public static void main(String[] args) {
-	// write your code here
-    }
-}
--- src/com/allen_anker/part1/APlusBFormat.java
+++ src/com/allen_anker/part1/APlusBFormat.java
@@ -1,5 +1,9 @@
 package com.allen_anker.part1;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class APlusBFormat {
     /**
      * Calculate a+b and output the sum in standard format -- that is,
@@ -7,7 +11,24 @@ public class APlusBFormat {
      *
      * @param args
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String[] numsStr = bufferedReader.readLine().split(" ");
+        bufferedReader.close();
 
+        int num1 = Integer.parseInt(numsStr[0]), num2 = Integer.parseInt(numsStr[1]);
+        char[] chars = String.valueOf(num1 + num2).toCharArray();
+        StringBuilder sb = new StringBuilder();
+        int count = 0;
+        int length = chars.length;
+        for (int i = length - 1; i >= 0; i--, count++) {
+            if (count == 3 && chars[i] != '-') {
+                sb.append(',');
+                count = 0;
+            }
+            sb.append(chars[i]);
+        }
+
+        System.out.println(sb.reverse().toString());
     }
 }
