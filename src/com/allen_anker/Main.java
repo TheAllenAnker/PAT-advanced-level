dc97a1210f61898637b003d7bd47709257118d55
TheAllenAnker <theallenanker@gmail.com>
2019-03-16 15:09:31 +0800

    Initial commit.

---
+++ src/com/allen_anker/Main.java
@@ -0,0 +1,8 @@
+package com.allen_anker;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
---
+++ src/com/allen_anker/part1/APlusBFormat.java
@@ -0,0 +1,13 @@
+package com.allen_anker.part1;
+
+public class APlusBFormat {
+    /**
+     * Calculate a+b and output the sum in standard format -- that is,
+     * the digits must be separated into groups of three by commas (unless there are less than four digits).
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+
+    }
+}
